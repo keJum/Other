e6a3d3051ee843b14177d632d79d61f55b5bde14
kejum <amkejum@gmail.com>
2020-10-10 19:19:47 +0400

    Правки практической работы №5

--- python/exspress/practics/6/python.py
+++ python/exspress/practics/6/python.py
@@ -3,14 +3,13 @@ import string
 with open("moby_01.txt") as infile, open("moby_01_clean.txt", "w") as outfile:
     for line in infile:
         line = line.lower()
-        punct_list = list(string.punctuation)
+        punt_list = list(string.punctuation)
         word_line = list(line)
-        for punct in punct_list:
-            for i in range(word_line.count(punct)):
-                word_line.remove(punct)
+        for punt in punt_list:
+            for i in range(word_line.count(punt)):
+                word_line.remove(punt)
         line = "".join(word_line)
         line = line.strip()
         clean_words = line.split(" ")
-        print(clean_words)
         for clean_word in clean_words:
             outfile.write(clean_word+'\n')
