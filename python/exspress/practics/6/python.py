61a851fbf34c95acf12323f0b70711493f5642dd
kejum <amkejum@gmail.com>
2020-10-10 19:48:06 +0400

    Правки практической работы №5

--- python/exspress/practics/6/python.py
+++ python/exspress/practics/6/python.py
@@ -1,15 +1,17 @@
 import string
 
+delete_list = list(string.punctuation)
+
 with open("moby_01.txt") as infile, open("moby_01_clean.txt", "w") as outfile:
     for line in infile:
         line = line.lower()
-        punt_list = list(string.punctuation)
+        line = line.rstrip('/n')
         word_line = list(line)
-        for punt in punt_list:
-            for i in range(word_line.count(punt)):
-                word_line.remove(punt)
+        for delete_char in delete_list:
+            while word_line.count(delete_char):
+                word_line.remove(delete_char)
         line = "".join(word_line)
         line = line.strip()
         clean_words = line.split(" ")
         for clean_word in clean_words:
-            outfile.write(clean_word+'\n')
+            outfile.write(clean_word+' ')
